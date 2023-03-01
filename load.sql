8768801adc1e11158965f9d7d807e12f8971f81b
youweiyu <yu.youw@northeastern.edu>
2023-02-28 20:24:08 -0800

    refactor: change repo structure

---
+++ load.sql
@@ -0,0 +1,58 @@
+LOAD DATA INFILE 'users.csv' INTO TABLE YelpUsers
+  FIELDS TERMINATED BY ','
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES;
+
+LOAD DATA INFILE 'business.csv' INTO TABLE Restaurants
+  FIELDS TERMINATED BY ','
+  OPTIONALLY ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (RestaurantId,Name,Address,City,State,Zip,Latitude,Longitude,Stars,@dummy,@dummy);
+  
+LOAD DATA local INFILE "airbnb_listings_usa.csv" INTO TABLE Airbnb
+  # Fields are not quoted.
+  FIELDS TERMINATED BY ','
+  OPTIONALLY ENCLOSED BY '"'
+  # Windows platforms may need '\r\n'.#  # Windows platforms may need '\r\n'.
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (@dummy,AirbnbId, Name,@dummy,@dummy,@dummy,Neighborhood,latitude,longitude,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy, State, City);
+
+LOAD DATA INFILE 'hosts.csv' INTO TABLE Host
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\r\n'
+  IGNORE 1 LINES;
+  
+LOAD DATA INFILE 'review_small.csv' INTO TABLE Reviews
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\r\n'
+  IGNORE 1 LINES;
+  
+LOAD DATA INFILE 'tip.csv' IGNORE INTO TABLE Tips
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\r\n'
+  IGNORE 1 LINES
+  (UserId,RestaurantId,Compliment_count,Date,Context);
+
+
+LOAD DATA INFILE 'hours.csv'  INTO TABLE Hours
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\r\n'
+  IGNORE 1 LINES; 
+
+LOAD DATA INFILE 'attributes_new.csv'  INTO TABLE Attributes
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\r\n'
+  IGNORE 1 LINES; 
+  
+LOAD DATA INFILE 'category.csv'  INTO TABLE categories
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\r\n'
+  IGNORE 1 LINES; 
---
+++ questions.sql
@@ -0,0 +1,27 @@
+# 10 questions (draft):
+
+
+# user: 
+# [Youyu] List top 10 people who wrote the most reviews?
+
+# [Youwei] What users have created reviews for Asian restaurants? Provide username and timestamps. 
+
+
+# restaurants 
+# [YuYang]What cities in the US have the most number of restaurants with average ratings of 4 and above? 
+
+# [YuYang]What are the top 10 rated restaurants in Seattle? 
+
+# [YiCheng]What are the top 100 tipped Asian restaurants since Jan 2022?
+
+# [Youwei] Which restaurant type is the most popular based on the number of reviews. 
+
+# [Caihong]For each restaurant type, what is the ratio of bad reviews (rating of 2 and below) to all of the reviews.
+
+# [Min]What is the percentage of restaurants opening 24/7?
+
+
+# location related 
+# [Min]How many restaurants are within 1-mile walking distance of our Airbnb location and have a rating of 4.5 or higher?
+
+# [Min]What are the top 10 vegetarian restaurants in the Greater Seattle Area?
