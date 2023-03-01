d4940722e0e5a5388dc9d28c5a1a1827718db1da
Kurokoi <driver2223@gmail.com>
2023-02-28 21:19:41 -0800

    Update questions.sql

--- questions.sql
+++ questions.sql
@@ -4,8 +4,13 @@
 # user: 
 # [Youyu] List top 10 people who wrote the most reviews?
 
-# [Youwei] What users have created reviews for Asian restaurants? Provide username and timestamps. 
-
+# [Youwei] What users have created tips for Asian restaurants? Provide username and counts. 
+SELECT UserName, COUNT(Tips.UserId) AS CNT FROM Tips
+  LEFT JOIN YelpUsers ON Tips.UserId = YelpUsers.UserId
+  LEFT JOIN Categories ON Tips.RestaurantId = Categories.RestaurantId
+  WHERE categories like "%Asian%"
+  GROUP BY Tips.UserId
+  ORDER BY CNT DESC;
 
 # restaurants 
 # [YuYang]What cities in the US have the most number of restaurants with average ratings of 4 and above? 
@@ -14,7 +19,21 @@
 
 # [YiCheng]What are the top 100 tipped Asian restaurants since Jan 2022?
 
-# [Youwei] Which restaurant type is the most popular based on the number of reviews. 
+# [Youwei] List the type of the top 10 most popular restaurant based on the total number of reviews and tips. 
+SELECT Restaurants.Name, Categories, SUM(CNT) AS TOTAL FROM
+  (SELECT RestaurantId,COUNT(*) AS CNT FROM Reviews
+     GROUP BY RestaurantId
+   UNION ALL
+   SELECT RestaurantId,COUNT(*) AS CNT FROM Tips
+     GROUP BY RestaurantId) AS ReviewTip 
+  LEFT JOIN Categories 
+    ON ReviewTip.RestaurantId = Categories.RestaurantId
+  LEFT JOIN Restaurants
+    ON ReviewTip.RestaurantId = Restaurants.RestaurantId
+  WHERE categories like "%Restaurant%"
+  GROUP BY ReviewTip.RestaurantId
+  ORDER BY TOTAL DESC
+  LIMIT 10;
 
 # [Caihong]For each restaurant type, what is the ratio of bad reviews (rating of 2 and below) to all of the reviews.
 
