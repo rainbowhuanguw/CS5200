9dda2653761c8c2e3d93803f86464ce35e15dfec
minleu94 <71369597+minleu94@users.noreply.github.com>
2023-03-01 17:39:19 -0800

    Add files via upload

--- AirEats.sql
+++ AirEats.sql
@@ -1,7 +1,6 @@
 # Create the schema if necessary.
 CREATE SCHEMA IF NOT EXISTS AirEats;
 USE AirEats;
-
 DROP TABLE IF EXISTS Reviews;
 DROP TABLE IF EXISTS Tips;
 DROP TABLE IF EXISTS Hours;
@@ -13,8 +12,6 @@ DROP TABLE IF EXISTS Airbnb;
 DROP TABLE IF EXISTS Host;
 DROP TABLE IF EXISTS Recommendations;
 DROP TABLE IF EXISTS Restaurants;
-
-
 CREATE TABLE YelpUsers (
   UserId VARCHAR(22),
   UserName VARCHAR(255),
@@ -24,33 +21,30 @@ CREATE TABLE YelpUsers (
   funny int,
   cool int,
   fans int,
-  average_stars decimal(4,2),
+  average_stars decimal(4, 2),
   compliment_hot int,
   compliment_more int,
-  compliment_profile int, 
-  compliment_cute int, 
+  compliment_profile int,
+  compliment_cute int,
   compliment_list int,
-  compliment_note int, 
+  compliment_note int,
   compliment_plain int,
   compliment_cool int,
   compliment_funny int,
-  compliment_writer int, 
+  compliment_writer int,
   compliment_photos int,
   CONSTRAINT pk_YelpUsers_UserId PRIMARY KEY (UserId)
 );
-
-
 CREATE TABLE Airbnb (
   AirbnbId INT,
   Name VARCHAR(255),
   City VARCHAR(255),
   Neighborhood VARCHAR(255),
   State VARCHAR(22),
-  Latitude DECIMAL(10,7),
-  Longitude DECIMAL(10,7),
+  Latitude DECIMAL(10, 7),
+  Longitude DECIMAL(10, 7),
   CONSTRAINT pk_Airbnb_AirbnbId PRIMARY KEY (AirbnbId)
 );
-
 CREATE TABLE Users (
   UserName VARCHAR(255),
   FirstName VARCHAR(255),
@@ -59,17 +53,13 @@ CREATE TABLE Users (
   Phone VARCHAR(20),
   AirbnbId INT,
   CONSTRAINT pk_Users_UserName PRIMARY KEY (UserName),
-  CONSTRAINT fk_Airbnb_AirbnbId FOREIGN KEY (AirbnbId)
-		REFERENCES Airbnb(AirbnbId)
-		ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_Airbnb_AirbnbId FOREIGN KEY (AirbnbId) REFERENCES Airbnb(AirbnbId) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 CREATE TABLE Host (
   HostId Int,
   HostName VARCHAR(255),
   CONSTRAINT pk_Host_HostId PRIMARY KEY (HostId)
 );
-
 CREATE TABLE Restaurants(
   RestaurantId VARCHAR(22),
   Name VARCHAR(255),
@@ -77,30 +67,25 @@ CREATE TABLE Restaurants(
   City VARCHAR(255),
   State VARCHAR(10),
   Zip VARCHAR(10),
-  Latitude DECIMAL(10,7),
-  Longitude DECIMAL(10,7),
-  Stars DECIMAL(2,1),
+  Latitude DECIMAL(10, 7),
+  Longitude DECIMAL(10, 7),
+  Stars DECIMAL(2, 1),
   CONSTRAINT pk_Restaurant_RestaurantId PRIMARY KEY (RestaurantId)
 );
-
 CREATE TABLE Reviews (
   ReviewId VARCHAR(255),
   UserId VARCHAR(255),
   RestaurantId VARCHAR(255),
   Stars INT,
   #Might remove content field
-  Useful DECIMAL(7,1) NOT NULL,
-  Funny DECIMAL(7,1) NOT NULL,
-  Cool DECIMAL(7,1) NOT NULL,
+  Useful DECIMAL(7, 1) NOT NULL,
+  Funny DECIMAL(7, 1) NOT NULL,
+  Cool DECIMAL(7, 1) NOT NULL,
   Content TEXT,
   Date datetime,
   CONSTRAINT pk_Reviews_ReviewId PRIMARY KEY (ReviewId),
-  CONSTRAINT fk_Reviews_RestaurantId FOREIGN KEY (RestaurantId)
-    REFERENCES Restaurants(RestaurantId)
-    ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_Reviews_RestaurantId FOREIGN KEY (RestaurantId) REFERENCES Restaurants(RestaurantId) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-
 CREATE TABLE Tips(
   TipId INT AUTO_INCREMENT,
   UserId VARCHAR(22),
@@ -109,21 +94,15 @@ CREATE TABLE Tips(
   Date datetime,
   Context TEXT,
   CONSTRAINT pk_Tips_TipId PRIMARY KEY (TipId),
-  CONSTRAINT fk_Tips_RestaurantId FOREIGN KEY (RestaurantId)
-    REFERENCES Restaurants(RestaurantId)
-    ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_Tips_RestaurantId FOREIGN KEY (RestaurantId) REFERENCES Restaurants(RestaurantId) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 CREATE TABLE Recommendations(
   RecommendationId INT AUTO_INCREMENT,
   RestaurantId VARCHAR(22),
   UserId VARCHAR(22),
   CONSTRAINT pk_Recommendations_RecommendationId PRIMARY KEY (RecommendationId),
-  CONSTRAINT fk_Recommendations_RestaurantId FOREIGN KEY (RestaurantId)
-    REFERENCES Restaurants(RestaurantId)
-    ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_Recommendations_RestaurantId FOREIGN KEY (RestaurantId) REFERENCES Restaurants(RestaurantId) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 CREATE TABLE Hours(
   RestaurantId VARCHAR(255),
   Monday VARCHAR(255),
@@ -134,99 +113,17 @@ CREATE TABLE Hours(
   Saturday VARCHAR(255),
   Sunday VARCHAR(255),
   CONSTRAINT pk_Hours_RestaurantId PRIMARY KEY (RestaurantId),
-  CONSTRAINT fk_Hours_RestaurantId FOREIGN KEY (RestaurantId)
-    REFERENCES Restaurants(RestaurantId)
-    ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_Hours_RestaurantId FOREIGN KEY (RestaurantId) REFERENCES Restaurants(RestaurantId) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 CREATE TABLE Attributes(
-	RestaurantId VARCHAR(255),
-    Attributes TEXT,
-    CONSTRAINT pk_Attributes_RestaurantId PRIMARY KEY (RestaurantId),
-    CONSTRAINT fk_Attributes_RestaurantId FOREIGN KEY (RestaurantId)
-		REFERENCES Restaurants(RestaurantId)
-		ON UPDATE CASCADE ON DELETE CASCADE
+  RestaurantId VARCHAR(255),
+  Attributes TEXT,
+  CONSTRAINT pk_Attributes_RestaurantId PRIMARY KEY (RestaurantId),
+  CONSTRAINT fk_Attributes_RestaurantId FOREIGN KEY (RestaurantId) REFERENCES Restaurants(RestaurantId) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 CREATE TABLE Categories(
   RestaurantId VARCHAR(255),
   Categories TEXT,
   CONSTRAINT pk_Categories_RestaurantId PRIMARY KEY (RestaurantId),
-  CONSTRAINT fk_Categories_RestaurantId FOREIGN KEY (RestaurantId)
-    REFERENCES Restaurants(RestaurantId)
-    ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_Categories_RestaurantId FOREIGN KEY (RestaurantId) REFERENCES Restaurants(RestaurantId) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-
-
-LOAD DATA INFILE 'users.csv' INTO TABLE YelpUsers
-  FIELDS TERMINATED BY ','
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES;
-
-LOAD DATA INFILE 'business.csv' INTO TABLE Restaurants
-  FIELDS TERMINATED BY ','
-  OPTIONALLY ENCLOSED BY '"'
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES
-  (RestaurantId,Name,Address,City,State,Zip,Latitude,Longitude,Stars,@dummy,@dummy);
-  
-LOAD DATA local INFILE "airbnb_listings_usa.csv" INTO TABLE Airbnb
-  # Fields are not quoted.
-  FIELDS TERMINATED BY ','
-  OPTIONALLY ENCLOSED BY '"'
-  # Windows platforms may need '\r\n'.#  # Windows platforms may need '\r\n'.
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES
-  (@dummy,AirbnbId, Name,@dummy,@dummy,@dummy,Neighborhood,latitude,longitude,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy, State, City);
-
-LOAD DATA INFILE 'hosts.csv' INTO TABLE Host
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES;
-  
-LOAD DATA INFILE 'review_small.csv' INTO TABLE Reviews
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES;
-  
- LOAD DATA INFILE 'tip.csv' IGNORE INTO TABLE Tips
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES
-  (UserId,RestaurantId,Compliment_count,Date,Context);
-
-
-LOAD DATA INFILE 'hours.csv'  INTO TABLE Hours
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES; 
-
-LOAD DATA INFILE 'attributes_new.csv'  INTO TABLE Attributes
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES; 
-  
-LOAD DATA INFILE 'category.csv'  INTO TABLE categories
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES; 
-
-select *
-from host;
-
-select count(*) as count_reviews
-from reviews;
-
-select count(*) as count_users
-from users;
-
-select count(*) as count_tips
-from tips;
-
