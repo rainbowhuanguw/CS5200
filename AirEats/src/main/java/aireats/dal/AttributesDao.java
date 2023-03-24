6c242c6b039941c5ed1e29e3494e7a37260a71d4
youweiyu <yu.youw@northeastern.edu>
2023-03-23 19:40:25 -0700

    fix: fix import in AttributesDao

--- AirEats/src/main/java/aireats/dal/AttributesDao.java
+++ AirEats/src/main/java/aireats/dal/AttributesDao.java
@@ -3,6 +3,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import aireats.model.*;
 
